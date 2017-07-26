e3b58702b87c830f123417ae550139bce0f649fe
ranran <931094193@qq.com>
2017-07-26 15:04:25 +0800

    modify

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,3 @@
-package PACKAGE_NAME;
-
 /**
  * @author ranran
  * @version V1.0
@@ -9,4 +7,12 @@ package PACKAGE_NAME;
  * @date 2017/7/26 14:56
  */
 public class Test {
+
+    public static void main(String[] args) {
+        String nowTime = RDate.getNowStrTime();
+        System.out.println(nowTime);
+        System.out.println(RDate.getLastDayBegin());
+        System.out.println(RDate.getWeekBeginString());
+    }
+
 }
